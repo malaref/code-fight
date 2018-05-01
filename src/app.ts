edad48ad12d5c3d2b3bc735ec7137d92b070fe80
muhammad-alaref <muhammad.alaref96@eng-st.cu.edu.eg>
2018-05-02 00:33:48 +0200

    Added scripts and packages!

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import { createServer, Server } from 'http';
-import express from 'express';
-import socketIo from 'socket.io';
+import { createServer, Server } from "http";
+import express from "express";
+import socketIo from "socket.io";
 import path from "path";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const io = socketIo();
-io.on('connect', (socket: socketIo.Socket) => {
-    console.log('Connected client on port %s.', PORT);
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
+io.on("connect", (socket: socketIo.Socket) => {
+    console.log("Connected client on port %s.", PORT);
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
     });
 });
 
