b8e491153bccdeee5e4dff9b72a1bab83a78f15f
muhammad-alaref <muhammad.alaref96@eng-st.cu.edu.eg>
2018-05-03 04:05:51 +0200

    Added Ace!

---
+++ src/public/js/ide.ts
@@ -0,0 +1,6 @@
+$(function() {
+    const editor = ace.edit("editor");
+    editor.setTheme("ace/theme/dracula");
+    editor.session.setMode("ace/mode/python");
+    editor.setFontSize("20px");
+  });
